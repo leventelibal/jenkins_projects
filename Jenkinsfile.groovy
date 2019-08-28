0f1118f72cbcea6a940b696c854091f34dc7c0c6
leventelibal <leventelibal@gmail.com>
2019-08-28 19:47:16 -0400

    add parameter

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,12 +1,18 @@
 node{
-    stage("Pull Repo"){
+    stage("update Jenkins"){
         properties([parameters([string(defaultValue: '34.255.1.240', description: 'Please provide IP', name: 'Environment ', trim: true)])])
+        sh "Echo Parameter added"
+    }
+    stage ("Install Git"){
+        sh "sudo yum install git -y"
+    }
+    stage("Pull Repo"){
         git "https://github.com/miguelgrinberg/flask-examples.git"
     }
     stage("Install Requirments"){
-        sh "virtualenv /tmp/venv"
-        sh ". venv/bin/activate"
-        
+        //sh "virtualenv /tmp/venv"
+        //sh ". venv/bin/activate"
+        sh "echo Hello"
     }
     stage("Pip Install"){
         sh "pip install -r requirements.txt"
