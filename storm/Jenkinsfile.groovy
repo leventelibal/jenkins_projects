ba6cec6182e61249850b9fcf923cb40a6532ce13
leventelibal <leventelibal@gmail.com>
2019-08-28 21:25:00 -0400

    add script kill

--- storm/Jenkinsfile.groovy
+++ storm/Jenkinsfile.groovy
@@ -6,6 +6,12 @@ node{
     stage ("Install Git"){
         sh "ssh ec2-user@${ENVIR} sudo yum install git python-pip -y"
     }
+    stage("Remove Repo"){
+        sh "ssh ec2-user@${ENVIR} sudo rm -rf /home/ec2-user/Flaskex"
+    }
+    stage("Remove PID"){
+        sh "ssh  ec2-user@${ENVIR} sudo kill $(sudo lsof -i:5000   | awk '{print $2}' | grep [[:digit:]]) "
+    }
     stage("Pull Repo"){
         sh "ssh ec2-user@${ENVIR}  git clone https://github.com/leventelibal/stormpath-flask-sample.git 2> /dev/null"
     }
